6b718b78454810eb62589cbe0bdf43c974f3099f
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-08 23:25:40 -0500

    Update App.jsx

--- Frontend/Generation/src/App.jsx
+++ Frontend/Generation/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useMusicPlayer from "./musicPlayer";
+import musicURL from "../src/utils/music.mp3";
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useMusicPlayer(musicURL);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <NavBar />
+      <AnimatedRoutes />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
