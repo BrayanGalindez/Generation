3361319da30fddf25f0fbf532c11313d5b10f5d9
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-08 23:25:55 -0500

    Create CardData.jsx

---
+++ Frontend/Generation/src/data/CardData.jsx
@@ -0,0 +1,22 @@
+export const cardData = [
+  {
+    id: 1,
+    title: "Experiencia de usuario mejorada: ",
+    content: "Al tener un control completo sobre el frontend, podremos diseñar interfaces de usuario atractivas e intuitivas que faciliten la navegación y la comprensión de la información para los usuarios extraterrestres y las empresas intergalácticas. Esto impulsará la participación y la satisfacción del usuario, promoviendo así un mayor compromiso comercial.",
+  },
+  {
+    id: 2,
+    title: "Eficiencia en el procesamiento de datos:",
+    content: "Mediante el desarrollo Full-Stack, seremos capaces de diseñar y gestionar eficientemente bases de datos que almacenen y procesen la información comercial. Esto permitirá un acceso rápido y preciso a los datos relevantes, lo que impulsará la toma de decisiones informada y eficiente tanto para los extraterrestres como para las empresas intergalácticas.",
+  },
+  {
+    id: 3,
+    title: "Integración de tecnologías avanzadas:",
+    content: "Aprovechando las tecnologías similares existentes, como internet, ordenadores y computadoras, podremos utilizar herramientas y marcos de trabajo modernos en el desarrollo Full-Stack. Esto incluye el uso de frameworks y bibliotecas populares, como React o Angular en el frontend, y Node.js o Django en el backend. Estas tecnologías avanzadas nos permitirán crear aplicaciones web rápidas, seguras y altamente escalables.",
+  },
+  {
+    id: 4,
+    title: "Adaptabilidad a las necesidades comerciales cambiantes: ",
+    content: "La metodología Full-Stack nos brinda la flexibilidad para adaptarnos a las demandas y requisitos cambiantes del comercio intergaláctico. Podremos actualizar y expandir las funcionalidades de las páginas web de manera ágil, asegurando que las empresas intergalácticas puedan adaptarse rápidamente a las necesidades de los extraterrestres y viceversa.",
+  }
+];
