3e994097681eccb16ca75089f62c4775829e1148
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-08 23:25:44 -0500

    Create AnimatedRoutes.jsx

---
+++ Frontend/Generation/src/components/AnimatedRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "../pages/Home";
+import CardPage from "../pages/CardPage";
+import { AnimatePresence } from "framer-motion";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" />
+        <Route path="inicio" element={<Home />} />
+        <Route path="informacion" element={<CardPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default AnimatedRoutes;
