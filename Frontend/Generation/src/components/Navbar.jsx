db36049a3e76751c67b514ab65fce65e0ecae8c1
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-08 23:25:53 -0500

    Create Navbar.jsx

---
+++ Frontend/Generation/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import useMusicPlayer from "../musicPlayer";
+import musicURL from "../utils/music.mp3";
+import "../styles/components/NavBar.css";
+import { useNavigate } from "react-router-dom";
+
+function NavBar() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/inicio"); // Reemplaza "/otra-ruta" con la ruta a la que deseas redirigir
+  };
+  const { setVolume, playMusic, pauseMusic, volume, isPlaying } =
+    useMusicPlayer(musicURL);
+
+  const handleVolumeChange = (event) => {
+    const newVolume = event.target.value;
+    setVolume(newVolume);
+  };
+
+  const handleTogglePlay = () => {
+    if (isPlaying) {
+      pauseMusic();
+    } else {
+      playMusic();
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <button className="button-inicio" onClick={handleButtonClick}>
+        Inicio
+      </button>
+      <div className="controlsContainer">
+        <div className="playButton" onClick={handleTogglePlay}>
+          {isPlaying ? (
+            <AiOutlinePauseCircle className="playIcon" />
+          ) : (
+            <AiOutlinePlayCircle className="playIcon" />
+          )}
+        </div>
+
+        <div className="volumeControl">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default NavBar;
