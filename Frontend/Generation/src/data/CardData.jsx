d8b0788c962e7708872bd502048da286c2dfb9da
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-10 22:25:01 -0500

    Update CardData.jsx

--- Frontend/Generation/src/data/CardData.jsx
+++ Frontend/Generation/src/data/CardData.jsx
@@ -1,21 +1,26 @@
 export const cardData = [
   {
     id: 1,
+    title: "Que es el desarrollo Full-Stack ",
+    content: "El desarrollo Full-Stack se refiere a la capacidad de un desarrollador para trabajar en todas las capas de una aplicación web, incluyendo tanto el frontend como el backend, así como la gestión de bases de datos. Esta metodología integral nos permitirá abordar de manera eficiente y efectiva los desafíos comerciales en el ámbito intergaláctico.",
+  },
+  {
+    id: 2,
     title: "Experiencia de usuario mejorada: ",
     content: "Al tener un control completo sobre el frontend, podremos diseñar interfaces de usuario atractivas e intuitivas que faciliten la navegación y la comprensión de la información para los usuarios extraterrestres y las empresas intergalácticas. Esto impulsará la participación y la satisfacción del usuario, promoviendo así un mayor compromiso comercial.",
   },
   {
-    id: 2,
+    id: 3,
     title: "Eficiencia en el procesamiento de datos:",
     content: "Mediante el desarrollo Full-Stack, seremos capaces de diseñar y gestionar eficientemente bases de datos que almacenen y procesen la información comercial. Esto permitirá un acceso rápido y preciso a los datos relevantes, lo que impulsará la toma de decisiones informada y eficiente tanto para los extraterrestres como para las empresas intergalácticas.",
   },
   {
-    id: 3,
+    id: 4,
     title: "Integración de tecnologías avanzadas:",
     content: "Aprovechando las tecnologías similares existentes, como internet, ordenadores y computadoras, podremos utilizar herramientas y marcos de trabajo modernos en el desarrollo Full-Stack. Esto incluye el uso de frameworks y bibliotecas populares, como React o Angular en el frontend, y Node.js o Django en el backend. Estas tecnologías avanzadas nos permitirán crear aplicaciones web rápidas, seguras y altamente escalables.",
   },
   {
-    id: 4,
+    id: 5,
     title: "Adaptabilidad a las necesidades comerciales cambiantes: ",
     content: "La metodología Full-Stack nos brinda la flexibilidad para adaptarnos a las demandas y requisitos cambiantes del comercio intergaláctico. Podremos actualizar y expandir las funcionalidades de las páginas web de manera ágil, asegurando que las empresas intergalácticas puedan adaptarse rápidamente a las necesidades de los extraterrestres y viceversa.",
   }
